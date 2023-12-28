475d69f4712dba7c4d5f6dafb66b43b0837aa36d
ivanryzov <145494091+ivanryzov@users.noreply.github.com>
2023-12-28 13:38:09 +0300

    Lab 12

--- labs/Lab_12_python_intro/solution/2.py
+++
@@ -1,8 +0,0 @@
-n, X, medians, sum = int(input("Введите n ")), list(map(int, input("Введите последовательность чисел X: ").split())), [], 0
-for i in range(n):
-    X[:i+1]= sorted(X[:i+1])
-    if (i + 1) % 2 == 1: median = X[(i + 1) // 2]
-    else:median = X[i // 2]
-    medians.append(median)
-for median in medians: sum += median
-print("\nСумма медиан:", sum)   
